f03828b8143b5d8aaae141079c00f38e5609528d
Christian Estupinan <100639879+Ivesqui@users.noreply.github.com>
2025-08-24 15:30:41 -0500

    Subiendo archivos

---
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package holamundo;
+
+/**
+ *
+ * @author chris
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        var saludo  = "Hola mi nombre es ";
+        var nombre = "Christian";
+        String cadena = " y mi edad es ";
+        int edad = 27;
+        System.out.println(saludo + nombre + cadena + edad);
+        
+    }
+    
+}
